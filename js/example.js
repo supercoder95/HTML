141bc6e7ef5c204a30fe21ec5c474ba55180daf5
supercoder <kazehazaya@gmail.com>
2019-11-26 18:27:15 +0900

    first
    first push

---
+++ js/example.js
@@ -0,0 +1,10 @@
+function buttonClickEvent(event){
+    switch(event.target.id){
+        case 'button1' :
+            alert('버튼1');
+            break;
+        case 'button2' :
+            alert('버튼2');
+            break;
+    }
+}
